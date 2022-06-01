a38c2b35b14a04502cd09faec5d967aff969f884
AndreyPlakhov <96304171+AndreyPlakhov@users.noreply.github.com>
2022-06-01 19:51:46 +0200

    Eliminar case

--- calculadora/Model.java
+++ calculadora/Model.java
@@ -17,9 +17,7 @@ public class Model {
             return (number1 / number2);
         case "√":
             return sqrt(number1);
-        case "^":  
-            return Math.pow(number1, number2);
-        }
+                        }
         
         System.out.println("Operador desconhecido: " + operator);
         return 0;
